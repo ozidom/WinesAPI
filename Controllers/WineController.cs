81f7be9ceb4dabc491823d829633971a7ef428af
ozidom <dommillar@gmail.com>
2020-11-21 14:21:20 +1100

    initial commit

--- Controllers/WineController.cs
+++ Controllers/WineController.cs
@@ -12,7 +12,7 @@ namespace WinesAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",  "Scorching"
         };
 
         private readonly ILogger<WineForecastController> _logger;
